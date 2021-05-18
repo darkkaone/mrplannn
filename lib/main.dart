36192343ebab4d62f7a721e390cc2573c49ec248
darkkaone <77964744+darkkaone@users.noreply.github.com>
2021-05-18 09:26:00 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           child: MaterialApp(
             debugShowCheckedModeBanner: false,
             title: 'Material App',
-            initialRoute: 'graficas',
+            initialRoute: 'login',
             routes: {
               'login'    : ( BuildContext context ) => LoginPage(),
               'registro' : ( BuildContext context ) => RegistroPage(),
